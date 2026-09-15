34491f03a2f3228e832ba13161d65916ec07f975
shwet3333 <naikshwet2@gmail.com>
2026-09-15 14:27:37 -0500

    Add includes and enum to Reservationmanager.h

---
+++ Reservationmanager.h
@@ -0,0 +1,16 @@
+#ifndef RESERVATION_MANAGER_H
+#define RESERVATION_MANAGER_H
+
+#include "Reservation.h"
+#include "IReservationList.h"
+#include "IResourceCatalog.h"
+#include "IWaitingList.h"
+#include "ICancellationHistory.h"
+#include <string>
+
+enum class CreateResult {
+
+
+}
+
+#endif
