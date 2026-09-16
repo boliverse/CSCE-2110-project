f64648070555f9bafd362171cb4f90856b78947d
bryceharbi <cvf6pk8zrv@privaterelay.appleid.com>
2026-09-16 12:28:18 -0500

    Add LinkedList class with reservation methods

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,17 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
+#include "Reservation.h"
+
+using namespace std;
+
+class LinkedList
+{
+
+    private:
+        void InsertReservation(Reservation r1);
+        void RemoveReservation(Reservation r1);
+        void DisplayReservation(Reservation r1);
+        Reservation FindReservation(Reservation r1);
+};
+#endif
