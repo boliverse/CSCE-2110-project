e172ebc8051b3f76e35f1a785d7ae00971e71506
bryceharbi <cvf6pk8zrv@privaterelay.appleid.com>
2026-09-18 15:36:46 -0500

    Add WaitingList class with queue and dequeue methods
    Implemented queue and dequeue methods for managing student IDs in a waiting list.

--- WaitingList.cpp
+++ WaitingList.cpp
@@ -1,1 +1,62 @@
 #include "WaitingList.h"
+
+WaitingList::WaitingList()
+{
+    front = nullptr;
+    rear = nullptr;
+}
+
+WaitingList::~WaitingList()
+{
+    while(front != nullptr)
+    {
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+    }
+
+    rear = nullptr;
+}
+
+void WaitingList::queue(int studentID)
+{
+    Node* newNode = new Node(studentID);
+
+    if(rear == nullptr)
+    {
+        front = newNode;
+        rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+void WaitingList::dequeue()
+{
+    if(front == nullptr)
+    {
+        return;
+    }
+
+    Node* temp = front;
+    front = front->next;
+    delete temp;
+
+    if(front == nullptr)
+    {
+        rear = nullptr;
+    }
+}
+
+void WaitingList::display() const
+{
+    Node* current = front;
+
+    while(current != nullptr)
+    {
+        cout << "Student ID: " << current->studentID << endl;
+        current = current->next;
+    }
+}
