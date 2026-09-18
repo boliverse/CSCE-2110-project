48d01f922614e2151bdc4eca0e520ffbb6af52b2
bryceharbi <cvf6pk8zrv@privaterelay.appleid.com>
2026-09-18 15:34:04 -0500

    Define WaitingList class with queue operations

--- WaitingList.h
+++ WaitingList.h
@@ -1,8 +1,27 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef WAITINGLIST_H
+#define WAITINGLIST_H
 
 class WaitingList
 {
+private:
+    struct Node
+    {
+        int studentID;
+        Node* next;
+
+        Node(int id) : studentID(id), next(nullptr) {}
+    };
+
+    Node* front;
+    Node* rear;
+
+public:
+    WaitingList();
+    ~WaitingList();
+
+    void queue(int studentID);
+    void dequeue();
+    void display() const;
 };
 
 #endif
