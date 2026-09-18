4c7d7be73f81fc300e3df8dc90101fd769bc02c0
shwet3333 <naikshwet2@gmail.com>
2026-09-17 19:14:28 -0500

    Refactor Reservation class for ID handling
    Refactored Reservation class to use integers for IDs and improved constructor and getter/setter methods.

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,25 +1,86 @@
-#include "Reservation.h"
+#include "../include/Reservation.h"
 #include <iostream>
-#include <iomanip>
+
+using namespace std;
 
 Reservation::Reservation()
-  : reservationId(""), studentID(""), studentName(""), resourceID(""), reservationDate("") {}
+{
+    reservationID = 0;
+    studentID = 0;
+    studentName = "";
+    resourceID = 0;
+    reservationDate = "";
+}
 
-Reservation::Reservation(const std::string& reservationIDIn, const std::string& studentIDin, const std::string& studentNameIn, const std::string& resourceIdIn, const std::string& dateIn)
-  : reservationID(reservationIDIN), studentID(studentIDIn), studentNName(studentNameIN), resourceID(resourceIDIn), reservationDate(dateIn) {}
+Reservation::Reservation(int reservationID,
+                         int studentID,
+                         const string& studentName,
+                         int resourceID,
+                         const string& reservationDate)
+{
+    this->reservationID = reservationID;
+    this->studentID = studentID;
+    this->studentName = studentName;
+    this->resourceID = resourceID;
+    this->reservationDate = reservationDate;
+}
 
-std::string Reservation::getReservationID() const { return reservationID; }
-std::string Reservation::getStudentID() const {return studentID; }
-std::string Reservation::getStudentName() const { return studentName; }
-std::string Reservation::getResourceID() const { return resourceID; }
-std::string Reservation::getReservationDate() const { return reservationDate; }
+int Reservation::getReservationID() const
+{
+    return reservationID;
+}
 
-void Reservation::display() const{
-  std::cout << std::left
-            << std::setw(10) << reservationID
-            << std::setw(10) << studentID
-            << std::setw(18) << stdentName
-            << std::setw(10) << resourceID
-            << reservationDate
-            << std::endl;
+int Reservation::getStudentID() const
+{
+    return studentID;
+}
+
+string Reservation::getStudentName() const
+{
+    return studentName;
+}
+
+int Reservation::getResourceID() const
+{
+    return resourceID;
+}
+
+string Reservation::getReservationDate() const
+{
+    return reservationDate;
+}
+
+void Reservation::setReservationID(int id)
+{
+    reservationID = id;
+}
+
+void Reservation::setStudentID(int id)
+{
+    studentID = id;
+}
+
+void Reservation::setStudentName(const string& name)
+{
+    studentName = name;
+}
+
+void Reservation::setResourceID(int id)
+{
+    resourceID = id;
+}
+
+void Reservation::setReservationDate(const string& date)
+{
+    reservationDate = date;
+}
+
+void Reservation::display() const
+{
+    cout << "Reservation ID: " << reservationID << endl;
+    cout << "Student ID: " << studentID << endl;
+    cout << "Student Name: " << studentName << endl;
+    cout << "Resource ID: " << resourceID << endl;
+    cout << "Reservation Date: " << reservationDate << endl;
+    cout << "-----------------------------" << endl;
 }
